b6c5ad3f2ec059065ff14c2794927c8c245f173c
Handsome Mpofu <mpofuhc@yahoo.com>
2020-09-18 11:47:35 +0200

    Following changes made: -Declared a button -Added an import for the Button

--- app/src/main/java/com/example/gitexample/MainActivity.java
+++ app/src/main/java/com/example/gitexample/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.gitexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
